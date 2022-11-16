4d32391a40553dd0518697b4d71b08fb9468879a
Имя пользователя <@pochta.com>
2022-11-16 14:01:36 +0500

    Приветствие пользователей

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Лена")
+{
+    Console.WriteLine("Припет, плюшка!=)");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
